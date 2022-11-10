e25de1bb68026a12243622c6f2de87726258f404
Ilya Dmitriev <dmitrievilyawork@gmail.com>
2022-11-10 08:19:54 +0300

    assessmet page api connected

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,6 +1,1 @@
-export { Market } from "./Market/Market";
-export { Registration } from "./Registration/Registration";
-export { Autorisation } from "./Autorisation/Autorisation";
-export { PasswordRecover } from "./PasswordRecover/PasswordRecover";
-export { Assessment } from "./Assessment/Assessment";
-export { Settings } from "./Settings/Settings";
+export { Loader } from "./Loader/Loader";
